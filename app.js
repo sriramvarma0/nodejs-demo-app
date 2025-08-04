e207ca6a6a111e3d2b7ca9b25b7c625a13a173ad
SRIRAM_VARMA <2200032442@kluniversity.in>
2025-08-04 17:18:50 +0530

    Updated index.html with animated Coming Soon landing page

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello from Node.js App via CI/CD!'));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
 
 app.listen(port, () => {
   console.log(`App running at http://localhost:${port}`);
